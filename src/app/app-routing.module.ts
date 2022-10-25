5167b84398fe9e31b4de0e29eb41e416b38ca78f
FeMotta <fernandosantosmotta@gmail.com>
2022-10-25 20:40:12 -0300

    adiciona router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './public/blog/blog.component';
+import { HomeComponent } from './public/home/home.component';
+import { SobreComponent } from './public/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'sobre', component: SobreComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
