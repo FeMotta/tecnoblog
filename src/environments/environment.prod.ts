f15a2b3b8c4c42f746bf9fe257d3360ddd0b3d09
FeMotta <fernandosantosmotta@gmail.com>
2022-10-26 20:42:31 -0300

    adiciona evironment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'fire-tecnoblog',
+    appId: '1:158906827533:web:4ef0db2f92fcde13d86644',
+    storageBucket: 'fire-tecnoblog.appspot.com',
+    apiKey: 'AIzaSyDyL2pDAKMKZ6WeZngXQqEGB2zQ-Z26IiE',
+    authDomain: 'fire-tecnoblog.firebaseapp.com',
+    messagingSenderId: '158906827533',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'fire-tecnoblog',
+    appId: '1:158906827533:web:4ef0db2f92fcde13d86644',
+    storageBucket: 'fire-tecnoblog.appspot.com',
+    apiKey: 'AIzaSyDyL2pDAKMKZ6WeZngXQqEGB2zQ-Z26IiE',
+    authDomain: 'fire-tecnoblog.firebaseapp.com',
+    messagingSenderId: '158906827533',
+  },
   production: false
 };
 
