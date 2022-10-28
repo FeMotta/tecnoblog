7d6b41bf69cf5b5f8c00ea293b33666658fa0f07
FeMotta <fernandosantosmotta@gmail.com>
2022-10-28 00:07:09 -0300

    atualiza authservice

--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import firebase from 'firebase/compat/app';
+import { FirestoreService } from '../database/firestore.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private auth: AngularFireAuth, private router: Router) { }
+  constructor(private auth: AngularFireAuth, private router: Router, private firestoreService: FirestoreService) { }
 
   loginEmail(email: string, password: string) {
     this.auth.signInWithEmailAndPassword(email, password)
@@ -18,6 +20,30 @@ export class AuthService {
       .catch((error) => {
         console.log(error)
       })
+  }
+
+  loginWithGoogle() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then((user) => {
+        this.firestoreService.getUser(user.user?.uid).subscribe(data => {
+          if (data.length == 0) {
+            this.firestoreService.addUser(user.user?.uid, user.user?.email, user.user?.displayName, user.user?.photoURL).then(() => {
+              console.log('User added')
+            }).catch((error) => {
+              console.log(error)
+            })
+          }
+        })
+      })
+    }
+
+  isLogged() {
+    return this.auth.authState;
+  }
+
+  logout() {
+    this.auth.signOut();
+    this.router.navigate(['/'])
   }
 
 }
