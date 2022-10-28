c5dc9619d354d568ddf824e35b0fdc9cac205b5f
FeMotta <fernandosantosmotta@gmail.com>
2022-10-28 00:07:19 -0300

    atualiza database

--- src/app/shared/database/firestore.service.ts
+++ src/app/shared/database/firestore.service.ts
@@ -10,6 +10,8 @@ export class FirestoreService {
   constructor(private db: AngularFirestore) {
   }
 
+  // <--- CRUD de Noticias --->
+
   addNoticia(noticia: any) {
     return this.db.collection('noticias').add(noticia);
   }
@@ -46,6 +48,98 @@ export class FirestoreService {
         return { id, ...data };
       })
     );
+  }
+
+  updateNoticia(id: any, noticia: any) {
+    return this.db.collection('noticias').doc(id).update(noticia);
+  }
+
+  // <--- CRUD de Comentarios --->
+
+  addComentario(id: any, comentario: any) {
+    return this.db.collection('noticias').doc(id).collection('comentarios').add(comentario)
+  }
+
+  getComentarios(id: any) {
+    return this.db.collection('noticias').doc(id).collection('comentarios').snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as any;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }
+      ))
+    );
+  }
+
+  // pegar comentarios do usuario
+  getComentariosUser(id: any, uid: any) {
+    return this.db.collection('noticias').doc(id).collection('comentarios', ref => ref.where('uid', '==', uid)).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as any;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }
+      ))
+    );
+  }
+
+  deleteComentario(id: any, idComentario: any) {
+    return this.db.collection('noticias').doc(id).collection('comentarios').doc(idComentario).delete();
+  }
+
+  // <--- CRUD de Usuarios --->
+
+  addUser(id: any, email: any, displayName: any, photoURL: any) {
+    return this.db.collection('users').doc(id).set({
+      email: email,
+      displayName: displayName,
+      photoURL: photoURL
+    });
+  }
+
+  getUser(id: any) {
+    return this.db.collection('users', ref => ref.where('uid', '==', id)).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as any;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }
+      ))
+    );
+  }
+
+  // <--- CRUD de Curtidas --->
+
+  addCurtida(id: any, uid: any) {
+    return this.db.collection('noticias').doc(id).collection('curtidas').doc(uid).set({
+      uid: uid
+    });
+  }
+
+  getCurtidas(id: any) {
+    return this.db.collection('noticias').doc(id).collection('curtidas').snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as any;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }
+      ))
+    );
+  }
+
+  getUserCurtida(id: any, uid: any) {
+    return this.db.collection('noticias').doc(id).collection('curtidas', ref => ref.where('uid', '==', uid)).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as any;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }
+      ))
+    );
+  }
+
+  deleteCurtida(id: any, uid: any) {
+    return this.db.collection('noticias').doc(id).collection('curtidas').doc(uid).delete();
   }
 
 }
