d4a2d88e6794c5f4398a343abd45236f7a346e97
FeMotta <fernandosantosmotta@gmail.com>
2022-10-29 11:14:53 -0300

    adiciona spinner no load das noticias

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { Observable } from 'rxjs';
 import { FirestoreService } from 'src/app/shared/database/firestore.service';
 
@@ -12,15 +13,23 @@ export class HomeComponent implements OnInit {
   mobileNav = false;
   noticias: Observable<any> = new Observable<any>();
 
-  constructor(private firestoreService: FirestoreService) { }
+  constructor(private firestoreService: FirestoreService, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+    this.spinner.show();
     this.noticias = this.firestoreService.getNoticiasLimit(3);
+    this.closeSpinner();
   }
 
   toggleNavbar(): void {
     this.mobileNav = !this.mobileNav;
     console.log(this.mobileNav);
+  }
+
+  closeSpinner(): void {
+    this.noticias.subscribe(() => {
+      this.spinner.hide();
+    })
   }
 
 }
