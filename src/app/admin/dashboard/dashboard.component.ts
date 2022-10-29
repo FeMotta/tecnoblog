9c6823337020be68ccf960a2d8f6d4492457844b
FeMotta <fernandosantosmotta@gmail.com>
2022-10-28 22:27:24 -0300

    adiciona filtro de noticias para o dashboard

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs';
 import { FirestoreService } from 'src/app/shared/database/firestore.service';
 
 import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,12 +14,14 @@ import { ToastrService } from 'ngx-toastr';
 export class DashboardComponent implements OnInit {
 
   noticias: Observable<any> = new Observable<any>();
+  noticiasHoje: Observable<any> = new Observable<any>();
 
-  constructor(private firestoreService: FirestoreService, private toastr: ToastrService) { }
+  constructor(private firestoreService: FirestoreService, private toastr: ToastrService, private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.noticias = this.firestoreService.getNoticias();
-    console.log(this.noticias);
+    this.noticiasHoje = this.firestoreService.getNoticiasHoje();
+    this.auth.isAdmin().subscribe
   }
 
   deleta(id: string) {
