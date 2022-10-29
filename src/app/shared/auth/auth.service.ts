9d0724bce3e4ef963ebeefd07e186c3487fe5f26
FeMotta <fernandosantosmotta@gmail.com>
2022-10-28 22:31:34 -0300

    adiciona login github e checagem admin

--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import { GithubAuthProvider } from "firebase/auth";
 import firebase from 'firebase/compat/app';
 import { FirestoreService } from '../database/firestore.service';
+import { map } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private auth: AngularFireAuth, private router: Router, private firestoreService: FirestoreService) { }
+  constructor(private auth: AngularFireAuth, private router: Router, private firestoreService: FirestoreService ) { }
 
   loginEmail(email: string, password: string) {
     this.auth.signInWithEmailAndPassword(email, password)
@@ -31,11 +34,36 @@ export class AuthService {
               console.log('User added')
             }).catch((error) => {
               console.log(error)
-            })
-          }
-        })
+          })
+        }
       })
-    }
+    })
+  }
+
+  isAdmin() {
+    return this.auth.user.pipe(map(user => {
+      if (user?.email == 'admin@email.com' || 'fernandosantosmotta@gmail.com') {
+        this.router.navigate(['/admin/dashboard'])
+      } else {
+        this.router.navigate(['/'])
+      }
+    }))
+  }
+
+  loginWithGithub() {
+    this.auth.signInWithPopup(new GithubAuthProvider())
+      .then((user) => {
+        this.firestoreService.getUser(user.user?.uid).subscribe(data => {
+          if (data.length == 0) {
+            this.firestoreService.addUser(user.user?.uid, user.user?.email, user.user?.displayName, user.user?.photoURL).then(() => {
+              console.log('User added')
+            }).catch((error) => {
+              console.log(error)
+          })
+        }
+      })
+    })
+  }
 
   isLogged() {
     return this.auth.authState;
@@ -43,7 +71,6 @@ export class AuthService {
 
   logout() {
     this.auth.signOut();
-    this.router.navigate(['/'])
   }
 
 }
