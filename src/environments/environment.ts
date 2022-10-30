cddce9791ca1206de45b60cd1a2accc41342ef40
FeMotta <fernandosantosmotta@gmail.com>
2022-10-29 22:15:54 -0300

    atualiza env para analytics

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,11 @@ export const environment = {
     projectId: 'fire-tecnoblog',
     appId: '1:158906827533:web:4ef0db2f92fcde13d86644',
     storageBucket: 'fire-tecnoblog.appspot.com',
+    locationId: 'us-central',
     apiKey: 'AIzaSyDyL2pDAKMKZ6WeZngXQqEGB2zQ-Z26IiE',
     authDomain: 'fire-tecnoblog.firebaseapp.com',
     messagingSenderId: '158906827533',
+    measurementId: 'G-CF04ZWV89R',
   },
   production: false
 };
